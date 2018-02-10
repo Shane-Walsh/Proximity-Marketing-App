5c0a26b55ad8bb30097c6278c47ed388709db7d2
Shane Walsh <walsh.shane@icloud.com>
2018-02-10 15:23:23 +0000

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Proximity Marketing App</h1>
+        </header>
+        <p className="App-intro">
+            <img src="../images/shoe.png" className="Shoe-info" alt="Hurrican 16" />
+            <br/>
+            <img src="../images/demoreview.png" className="Review-info" alt="Review Info" />
+            <br/>
+            <img src="../images/pricecompare.png" className="Price-info" alt="Price Info" />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
